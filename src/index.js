f6dfa811f594d5a6d2990ee6fa00ae96bbf04dbc
dna737 <adnanhere27@gmail.com>
2023-07-12 13:03:05 -0500

    implement sanitizeInput()

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 //API key: d76698f4e34640fa8c434718230206
 async function retrieveWeatherInfo(location) {
+  let userInput = sanitizeInput(location);
   try {
     let apiResponse = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=d76698f4e34640fa8c434718230206&q=${location}=3`
+      `http://api.weatherapi.com/v1/forecast.json?key=d76698f4e34640fa8c434718230206&q=${userInput}&days=3`
     );
     let jsonData = await apiResponse.json();
-    console.log("data:", jsonData.forecast.forecastday[1].day.condition.text);
+    console.log("data:", jsonData);
+    // console.log("data:", jsonData.forecast.forecastday[1].day.condition.text);
   } catch (error) {
     console.log("error:", error);
   }
 }
 
-retrieveWeatherInfo();
+function sanitizeInput(input) {
+  if (!input) return "London";
+  return input;
+}
+
+retrieveWeatherInfo("");
