b30a4214768b6bec39be49d3f2374065027e407f
dna737 <adnanhere27@gmail.com>
2023-07-13 12:17:44 -0500

    split async functions into smaller ones

--- src/index.js
+++ src/index.js
@@ -2,35 +2,50 @@ import "./styles.css";
 
 const cardsContainer = document.querySelector(".cards-container");
 //API key: d76698f4e34640fa8c434718230206
-async function retrieveWeatherInfo(location) {
+async function hitWeatherApi(location) {
+  let apiResponse = null;
   try {
     let userInput = sanitizeInput(location);
 
-    let apiResponse = await fetch(
+    apiResponse = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=d76698f4e34640fa8c434718230206&q=${userInput}&days=3`
     );
+    return apiResponse;
+  } catch {
+    apiResponse = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=d76698f4e34640fa8c434718230206&q=London&days=3`
+    );
+    return apiResponse;
+  }
+}
+
+async function retrieveWeatherDetails(apiResponse) {
+  console.log(
+    "🚀 ~ file: index.js:23 ~ retrieveWeatherDetails ~ apiResponse:",
+    apiResponse
+  );
+  try {
     let jsonData = await apiResponse.json();
 
     let daysArray = jsonData.forecast.forecastday;
     let currLocation =
       jsonData.location.name + ", " + jsonData.location.country;
     console.log(
-      "🚀 ~ file: index.js:20 ~ retrieveWeatherInfo ~ location:",
+      "🚀 ~ file: index.js:20 ~ hitWeatherApi ~ location:",
       currLocation
     );
 
-    for (let i = 0; i < daysArray.length; i++) {
-      createWeatherCard(daysArray[i], i);
-    }
-
     return daysArray;
   } catch {
-    throw new Error("Resetting location to default...");
+    throw new Error("failed to parse JSON!");
   }
 }
 
 function sanitizeInput(input) {
-  if (!input) return "London";
+  if (!input) {
+    console.log("london");
+    return "London";
+  }
   return input;
 }
 
@@ -38,11 +53,14 @@ function activateSubmitButton() {
   const submitButton = document.querySelector(".submit-button");
   submitButton.addEventListener("click", () => {
     const inputValue = document.getElementById("search").value;
-    retrieveWeatherInfo(inputValue);
+    hitWeatherApi(inputValue);
+    retrieveWeatherDetails(apiResponse);
+    //FIXME: You can place the two functions above in another function called "updateLocation()" or something similar.
   });
 }
 
 function createWeatherCard(day, index) {
+  console.log("called craeteWeatherCard");
   //TODO: fetch the image area div using the index of the thing
   if (cardsContainer) {
     appendTopBar(day, index);
@@ -79,16 +97,19 @@ function appendImage(day, index) {
   imageDiv.appendChild(image);
 }
 
+function manageWeatherCards(data) {
+  //data is an array that contains the forecast details of three days.
+  for (let i = 0; i < data.length; i++) {
+    createWeatherCard(data[i], i);
+  }
+}
+
 //TODO: implement an IIFE that displays a default country and adds event listeners to the input field.
-(() => {
+(async () => {
   activateSubmitButton();
 
-  const daysArray = retrieveWeatherInfo()
-    .then(function (daysArray) {
-      for (let i = 0; i < daysArray.length; i++) {
-        createWeatherCard(daysArray[i], i);
-      }
-    })
-    //TODO: you need to be able to call the default location without creating a "hellish" thing. LOOK AT STEPS 2 AND 3.
-    .catch((error) => retrieveWeatherInfo());
+  //TODO: you need to be able to call the default location without creating a "hellish" thing. LOOK AT STEPS 2 AND 3.
+  let apiResponse = await hitWeatherApi();
+  let data = await retrieveWeatherDetails(apiResponse);
+  manageWeatherCards(data);
 })();
